000ae06f0db19854a38fae5ab0e8a5a2706a760d
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-06 10:54:33 +0900

    【追加】reactでの実装を意識したモックに変更 #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["う"]);
+
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div class="incomplete-area">
+        <p class="title">未完了TODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} class="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div class="complete-area">
+        <p class="title">完了したTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} class="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
